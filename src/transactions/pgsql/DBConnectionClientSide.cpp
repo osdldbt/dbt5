7fcf8babf095c4cc87ee54569c8c85049bc7f3c9
Mark Wong <markwkm@gmail.com>
2024-05-12 08:50:33 -0700

    pgsql: add customer position frame 1 client side logic

--- src/transactions/pgsql/DBConnectionClientSide.cpp
+++ src/transactions/pgsql/DBConnectionClientSide.cpp
@@ -76,6 +76,154 @@ void
 CDBConnectionClientSide::execute(const TCustomerPositionFrame1Input *pIn,
 		TCustomerPositionFrame1Output *pOut)
 {
+	ostringstream osSQL;
+	PGresult *res = NULL;
+
+	pOut->cust_id = pIn->cust_id;
+
+	if (pOut->cust_id == 0) {
+		osSQL << "SELECT c_id" << endl
+			  << "FROM   customer" << endl
+			  << "WHERE  c_tax_id = '" << pIn->tax_id << "'";
+		if (m_bVerbose) {
+			cout << osSQL.str() << endl;
+		}
+		res = exec(osSQL.str().c_str());
+
+		if (PQntuples(res) == 0) {
+			return;
+		}
+
+		pOut->cust_id = atoll(PQgetvalue(res, 0, 0));
+		PQclear(res);
+
+		if (m_bVerbose) {
+			cout << "c_id = " << pOut->cust_id << endl;
+		}
+	}
+
+	osSQL.clear();
+	osSQL.str("");
+	osSQL << "SELECT c_st_id" << endl
+		  << "     , c_l_name" << endl
+		  << "     , c_f_name" << endl
+		  << "     , c_m_name" << endl
+		  << "     , c_gndr" << endl
+		  << "     , c_tier" << endl
+		  << "     , c_dob" << endl
+		  << "     , c_ad_id" << endl
+		  << "     , c_ctry_1" << endl
+		  << "     , c_area_1" << endl
+		  << "     , c_local_1" << endl
+		  << "     , c_ext_1" << endl
+		  << "     , c_ctry_2" << endl
+		  << "     , c_area_2" << endl
+		  << "     , c_local_2" << endl
+		  << "     , c_ext_2" << endl
+		  << "     , c_ctry_3" << endl
+		  << "     , c_area_3" << endl
+		  << "     , c_local_3" << endl
+		  << "     , c_ext_3" << endl
+		  << "     , c_email_1" << endl
+		  << "     , c_email_2" << endl
+		  << "FROM customer" << endl
+		  << "WHERE c_id = " << pOut->cust_id;
+	if (m_bVerbose) {
+		cout << osSQL.str() << endl;
+	}
+	res = exec(osSQL.str().c_str());
+
+	if (PQntuples(res) == 0) {
+		return;
+	}
+
+	strncpy(pOut->c_st_id, PQgetvalue(res, 0, 0), cST_ID_len);
+	strncpy(pOut->c_l_name, PQgetvalue(res, 0, 1), cL_NAME_len);
+	strncpy(pOut->c_f_name, PQgetvalue(res, 0, 2), cF_NAME_len);
+	strncpy(pOut->c_m_name, PQgetvalue(res, 0, 3), cM_NAME_len);
+	strncpy(pOut->c_gndr, PQgetvalue(res, 0, 4), cGNDR_len);
+	pOut->c_tier = PQgetvalue(res, 0, 5)[0];
+	sscanf(PQgetvalue(res, 0, 6), "%hd-%hd-%hd", &pOut->c_dob.year,
+			&pOut->c_dob.month, &pOut->c_dob.day);
+	pOut->c_ad_id = atoll(PQgetvalue(res, 0, 7));
+	strncpy(pOut->c_ctry_1, PQgetvalue(res, 0, 8), cCTRY_len);
+	strncpy(pOut->c_area_1, PQgetvalue(res, 0, 9), cAREA_len);
+	strncpy(pOut->c_local_1, PQgetvalue(res, 0, 10), cLOCAL_len);
+	strncpy(pOut->c_ext_1, PQgetvalue(res, 0, 11), cEXT_len);
+	strncpy(pOut->c_ctry_2, PQgetvalue(res, 0, 12), cCTRY_len);
+	strncpy(pOut->c_area_2, PQgetvalue(res, 0, 13), cAREA_len);
+	strncpy(pOut->c_local_2, PQgetvalue(res, 0, 14), cLOCAL_len);
+	strncpy(pOut->c_ext_2, PQgetvalue(res, 0, 15), cEXT_len);
+	strncpy(pOut->c_ctry_3, PQgetvalue(res, 0, 16), cCTRY_len);
+	strncpy(pOut->c_area_3, PQgetvalue(res, 0, 17), cAREA_len);
+	strncpy(pOut->c_local_3, PQgetvalue(res, 0, 18), cLOCAL_len);
+	strncpy(pOut->c_ext_3, PQgetvalue(res, 0, 19), cEXT_len);
+	strncpy(pOut->c_email_1, PQgetvalue(res, 0, 20), cEMAIL_len);
+	strncpy(pOut->c_email_2, PQgetvalue(res, 0, 21), cEMAIL_len);
+	PQclear(res);
+
+	if (m_bVerbose) {
+		cout << "c_st_id = " << pOut->c_st_id << endl;
+		cout << "c_l_name = " << pOut->c_l_name << endl;
+		cout << "c_f_name = " << pOut->c_f_name << endl;
+		cout << "c_m_name = " << pOut->c_m_name << endl;
+		cout << "c_gndr = " << pOut->c_gndr << endl;
+		cout << "c_tier = " << pOut->c_tier << endl;
+		cout << "c_dob = " << pOut->c_dob.year << "-" << pOut->c_dob.month
+			 << "-" << pOut->c_dob.day << endl;
+		cout << "c_ad_id = " << pOut->c_ad_id << endl;
+		cout << "c_ctry_1 = " << pOut->c_ctry_1 << endl;
+		cout << "c_area_1 = " << pOut->c_area_1 << endl;
+		cout << "c_local_1 = " << pOut->c_local_1 << endl;
+		cout << "c_ext_1 = " << pOut->c_ext_1 << endl;
+		cout << "c_ctry_2 = " << pOut->c_ctry_2 << endl;
+		cout << "c_area_2 = " << pOut->c_area_2 << endl;
+		cout << "c_local_2 = " << pOut->c_local_2 << endl;
+		cout << "c_ext_2 = " << pOut->c_ext_2 << endl;
+		cout << "c_ctry_3 = " << pOut->c_ctry_3 << endl;
+		cout << "c_area_3 = " << pOut->c_area_3 << endl;
+		cout << "c_local_3 = " << pOut->c_local_3 << endl;
+		cout << "c_ext_3 = " << pOut->c_ext_3 << endl;
+		cout << "c_email_1 = " << pOut->c_email_1 << endl;
+		cout << "c_email_2 = " << pOut->c_email_2 << endl;
+	}
+
+	osSQL.clear();
+	osSQL.str("");
+	osSQL << "SELECT ca_id" << endl
+		  << "    ,  ca_bal" << endl
+		  << "    ,  coalesce(sum(hs_qty * lt_price), 0) AS soma" << endl
+		  << "FROM customer_account" << endl
+		  << "     LEFT OUTER JOIN holding_summary" << endl
+		  << "                  ON hs_ca_id = ca_id," << endl
+		  << "     last_trade" << endl
+		  << "WHERE ca_c_id = " << pOut->cust_id << endl
+		  << " AND lt_s_symb = hs_s_symb" << endl
+		  << "GROUP BY ca_id, ca_bal" << endl
+		  << "ORDER BY 3 ASC" << endl
+		  << "LIMIT 10";
+	if (m_bVerbose) {
+		cout << osSQL.str() << endl;
+	}
+	res = exec(osSQL.str().c_str());
+
+	pOut->acct_len = PQntuples(res);
+	for (int i = 0; i < pOut->acct_len; i++) {
+		pOut->acct_id[i] = atoll(PQgetvalue(res, i, 0));
+		pOut->cash_bal[i] = atof(PQgetvalue(res, i, 1));
+		pOut->asset_total[i] = atof(PQgetvalue(res, i, 2));
+	}
+	PQclear(res);
+
+	if (m_bVerbose) {
+		cout << "acct_len = " << pOut->acct_len << endl;
+		for (int i = 0; i < pOut->acct_len; i++) {
+			cout << "acct_id[" << i << "] = " << pOut->acct_id[i] << endl;
+			cout << "cash_bal[" << i << "] = " << pOut->cash_bal[i] << endl;
+			cout << "asset_total[" << i << "] = " << pOut->asset_total[i]
+				 << endl;
+		}
+	}
 }
 
 void
