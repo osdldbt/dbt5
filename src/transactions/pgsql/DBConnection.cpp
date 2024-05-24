034fdc16c6cc08638e08d1925898331b0628c302
Mark Wong <markwkm@gmail.com>
2024-05-24 11:25:16 -0700

    pgsql: refactor market feed for debugging

--- src/transactions/pgsql/DBConnection.cpp
+++ src/transactions/pgsql/DBConnection.cpp
@@ -147,6 +147,10 @@ CDBConnection::execute(const TMarketFeedFrame1Input *pIn,
 	strncpy(now_dts, PQgetvalue(res, 0, 0), 29);
 	PQclear(res);
 
+	if (m_bVerbose) {
+		cout << "now_dts = " << now_dts << endl;
+	}
+
 	pOut->num_updated = 0;
 	pOut->send_len = 0;
 
@@ -160,14 +164,21 @@ CDBConnection::execute(const TMarketFeedFrame1Input *pIn,
 			  << pIn->Entries[i].price_quote << ", lt_vol = lt_vol + "
 			  << pIn->Entries[i].trade_qty << ", lt_dts = '" << now_dts
 			  << "' WHERE lt_s_symb = '" << pIn->Entries[i].symbol << "'";
+		if (m_bVerbose) {
+			cout << osSQL.str() << endl;
+		}
 		res = exec(osSQL.str().c_str());
 		pOut->num_updated += atoi(PQcmdTuples(res));
 		PQclear(res);
 
 		osSQL.str("");
-		osSQL << "SELECT tr_t_id, tr_bid_price, tr_tt_id, tr_qty FROM "
-				 "trade_request WHERE tr_s_symb = '"
-			  << pIn->Entries[i].symbol << "' AND ((tr_tt_id = '"
+		osSQL << "SELECT tr_t_id" << endl
+			  << "     , tr_bid_price" << endl
+			  << "     , tr_tt_id" << endl
+			  << "     , tr_qty" << endl
+			  << "FROM trade_request" << endl
+			  << "WHERE tr_s_symb = '" << pIn->Entries[i].symbol
+			  << "' AND ((tr_tt_id = '"
 			  << pIn->StatusAndTradeType.type_stop_loss
 			  << "' AND tr_bid_price >= " << pIn->Entries[i].price_quote
 			  << ") OR (tr_tt_id = '"
@@ -176,33 +187,47 @@ CDBConnection::execute(const TMarketFeedFrame1Input *pIn,
 			  << ") OR (tr_tt_id = '" << pIn->StatusAndTradeType.type_limit_buy
 			  << "' AND tr_bid_price >= " << pIn->Entries[i].price_quote
 			  << "))";
+		if (m_bVerbose) {
+			cout << osSQL.str() << endl;
+		}
 		res = exec(osSQL.str().c_str());
 
-		int i_tr_t_id = 0;
-		int i_tr_bid_price = 1;
-		int i_tr_qty = 2;
-		int i_tr_tt_id = 3;
-
 		for (int j = 0; j < PQntuples(res); j++) {
+			if (m_bVerbose) {
+				cout << "t_id[" << j << "] = " << PQgetvalue(res, j, 0);
+				cout << "bid_price[" << j << "] = " << PQgetvalue(res, j, 1);
+				cout << "tt_id[" << j << "] = " << PQgetvalue(res, j, 2);
+				cout << "qty[" << j << "] = " << PQgetvalue(res, j, 3);
+			}
+
 			osSQL.str("");
 			osSQL << "UPDATE trade SET t_dts = '" << now_dts
 				  << "', t_st_id = '"
 				  << pIn->StatusAndTradeType.status_submitted
-				  << "' WHERE t_id = " << PQgetvalue(res, j, i_tr_t_id);
+				  << "' WHERE t_id = " << PQgetvalue(res, j, 0);
+			if (m_bVerbose) {
+				cout << osSQL.str() << endl;
+			}
 			res2 = exec(osSQL.str().c_str());
 			PQclear(res2);
 
 			osSQL.str("");
 			osSQL << "DELETE FROM trade_request WHERE tr_t_id = "
-				  << PQgetvalue(res, j, i_tr_t_id);
+				  << PQgetvalue(res, j, 0);
+			if (m_bVerbose) {
+				cout << osSQL.str() << endl;
+			}
 			res2 = exec(osSQL.str().c_str());
 			PQclear(res2);
 
 			osSQL.str("");
-			osSQL << "INSERT INTO trade_history VALUES ("
-				  << PQgetvalue(res, j, i_tr_t_id) << ", '" << now_dts
+			osSQL << "INSERT INTO trade_history" << endl
+				  << "VALUES (" << PQgetvalue(res, j, 0) << ", '" << now_dts
 				  << "', '" << pIn->StatusAndTradeType.status_submitted
 				  << "')";
+			if (m_bVerbose) {
+				cout << osSQL.str() << endl;
+			}
 			res2 = exec(osSQL.str().c_str());
 			PQclear(res2);
 		}
@@ -214,14 +239,24 @@ CDBConnection::execute(const TMarketFeedFrame1Input *pIn,
 		for (int j = 0; j < PQntuples(res); j++) {
 			strncpy(m_TriggeredLimitOrders.symbol, pIn->Entries[i].symbol,
 					cSYMBOL_len);
-			m_TriggeredLimitOrders.trade_id
-					= atoll(PQgetvalue(res, j, i_tr_t_id));
-			m_TriggeredLimitOrders.price_quote
-					= atof(PQgetvalue(res, j, i_tr_bid_price));
-			m_TriggeredLimitOrders.trade_qty
-					= atoi(PQgetvalue(res, j, i_tr_qty));
+			m_TriggeredLimitOrders.trade_id = atoll(PQgetvalue(res, j, 0));
+			m_TriggeredLimitOrders.price_quote = atof(PQgetvalue(res, j, 1));
 			strncpy(m_TriggeredLimitOrders.trade_type_id,
-					PQgetvalue(res, j, i_tr_tt_id), cTT_ID_len);
+					PQgetvalue(res, j, 2), cTT_ID_len);
+			m_TriggeredLimitOrders.trade_qty = atoi(PQgetvalue(res, j, 1));
+
+			if (m_bVerbose) {
+				cout << "symbol[" << j
+					 << "] = " << m_TriggeredLimitOrders.symbol;
+				cout << "trade_id[" << j
+					 << "] = " << m_TriggeredLimitOrders.trade_id;
+				cout << "price_quote[" << j
+					 << "] = " << m_TriggeredLimitOrders.price_quote;
+				cout << "trade_type_id[" << j
+					 << "] = " << m_TriggeredLimitOrders.trade_type_id;
+				cout << "trade_qty[" << j
+					 << "] = " << m_TriggeredLimitOrders.trade_qty;
+			}
 
 			bool bSent = pMarketExchange->SendToMarketFromFrame(
 					m_TriggeredLimitOrders);
