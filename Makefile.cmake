osdldbt/dbt5

commit 87a97b69c53111ce31f5c329e1edd887ebbd3426

AppImage: always include TPC-E Tools
Per TPC EULA v2.2 clause 9.d.  This also uses CMake features introduced
in v3.14.

Makefile.cmake
@@ -8,14 +8,12 @@ default:
 appimage:
 	cmake -H. -Bbuilds/appimage -DCMAKE_INSTALL_PREFIX=/usr
 	cd builds/appimage && make install DESTDIR=../AppDir
-	@if [ ! "$(EGEN)" = "" ]; then \
-		rm -rf builds/AppDir/opt/egen && \
-		mkdir -p builds/AppDir/opt/egen && \
-		unzip -d builds/AppDir/opt/egen "$(EGEN)" && \
-		builds/AppDir/usr/bin/dbt5-build-egen --include-dir=src/include \
-				--patch-dir=patches --source-dir=src \
-				builds/AppDir/opt/egen; \
-	fi
+	rm -rf builds/AppDir/opt/egen
+	mkdir -p builds/AppDir/opt/egen
+	cp -a builds/appimage/_deps/egen-src builds/AppDir/opt/egen
+	builds/AppDir/usr/bin/dbt5-build-egen --include-dir=src/include \
+			--patch-dir=patches --source-dir=src \
+			builds/AppDir/opt/egen; \
 	cd builds/appimage && make appimage
 
 clean:

CMakeLists.txt
@@ -1,4 +1,4 @@
-CMAKE_MINIMUM_REQUIRED(VERSION 3.5...4.0)
+CMAKE_MINIMUM_REQUIRED(VERSION 3.14...4.0)
 PROJECT(dbt5 NONE)
 
 set (PROJECT_VERSION_MAJOR 0)
@@ -16,6 +16,15 @@ set (
     HOMEPAGE
     "Database Test 5 (DBT-5) project page: https://github.com/osdldbt/dbt5"
 )
+
+include (FetchContent)
+FetchContent_Declare(
+    egen
+    GIT_REPOSITORY https://github.com/osdldbt/egen.git
+    GIT_TAG        1.14.0
+)
+FetchContent_MakeAvailable (egen)
+set (EGEN_SOURCE_DIR ${egen_SOURCE_DIR})
 
 add_subdirectory (appimage)
 add_subdirectory (patches)

appimage/AppRun.in
@@ -4,6 +4,7 @@
 usage() {
 	cat << EOF
 Database Test 5 (DBT-5) wrapped up in an AppImage.
+THE TPC SOFTWARE IS AVAILABLE WITHOUT CHARGE FROM TPC.
 
 Usage:
   $(basename "${0}") COMMAND [OPTION]

appimage/Containerfile.appimage
@@ -21,6 +21,7 @@ RUN apt-get -qq -y update && \
                            flex \
                            gcc \
                            g++ \
+                           git \
                            gnupg2 \
                            jq \
                            libcairo2-dev \
@@ -141,7 +142,7 @@ RUN mkdir -p /usr/local/share/licenses/rst2pdf-${RST2PDFVER} && \
        /usr/local/lib/python${PYTHONMMVER}/site-packages/docutils-${DOCUTILSVER}.dist-info/COPYING.txt \
        /usr/local/share/licenses/rst2pdf-${RST2PDFVER}/
 
-ARG CMAKEVER="3.7.2"
+ARG CMAKEVER="3.14.7"
 RUN curl -o /tmp/cmake-${CMAKEVER}-linux-${ARCH}.tar.gz -SsOL \
          https://github.com/Kitware/CMake/releases/download/v${CMAKEVER}/cmake-${CMAKEVER}-linux-${ARCH}.tar.gz && \
     tar -C /usr --strip-components=1 \

tools/build-appimage
@@ -15,13 +15,6 @@ CONTAINER_DIR="$(dirname "$(realpath "${0}")")"
 CONTAINER_TAG="dbt5-appimage"
 TARGET="appimage"
 
-if [ "${EGEN}" = "" ]; then
-    # Automatically detect and glob the first copy of TPC E Tools zip file in
-    # the project root.
-    EGEN=$(basename ${CONTAINER_DIR}/../*-tpc-e-tool.zip)
-    echo "Detected and building with ${EGEN}"
-fi
-
 # Use the return code from inspect to determine if the container image
 # needs to be created.
 if ! ${CONTAINER_CMD} inspect ${CONTAINER_TAG} > /dev/null 2>&1; then
@@ -39,7 +32,6 @@ ${CONTAINER_CMD} run \
 		--user "$(id -u):$(id -u)" \
 		${USERNS_ARG} \
 		-v "${CONTAINER_DIR}/..:/dbt5:rw,Z" \
-		--env EGEN="${EGEN}" \
 		--env PKG_CONFIG_PATH="/usr/lib/pkgconfig" \
 		-w /dbt5 \
 		$CONTAINER_TAG \

commit 9e50de19e7de212a14279d515912519ef26c9add

AppImage: Fix building AppImage with github.com/osdldbt/egen repo
Bug introduced by 87a97b69c53111ce31f5c329e1edd887ebbd3426, but egen
would still be included if it already existed from a previous build.
In other words only new or clean builds failed.

Makefile.cmake
@@ -10,7 +10,7 @@ appimage:
 	cd builds/appimage && make install DESTDIR=../AppDir
 	rm -rf builds/AppDir/opt/egen
 	mkdir -p builds/AppDir/opt/egen
-	cp -a builds/appimage/_deps/egen-src builds/AppDir/opt/egen
+	cp -a builds/appimage/_deps/egen-src/. builds/AppDir/opt/egen
 	builds/AppDir/usr/bin/dbt5-build-egen --include-dir=src/include \
 			--patch-dir=patches --source-dir=src \
 			builds/AppDir/opt/egen; \